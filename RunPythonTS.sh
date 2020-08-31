936874e32516721c480009494ab1d5768745ee2f
martinorr23 <55511450+martinorr23@users.noreply.github.com>
2020-08-31 21:28:25 +0100

    Update RunPythonTS.sh

--- RunPythonTS.sh
+++ RunPythonTS.sh
@@ -1,7 +1,7 @@
 chmod +x tsmapper.py
 chmod +x tscombiner.py
 chmod +x tsreducer.py
-hdfs dfs -rm /SSPProj
+hdfs dfs -rmdir /SSPProj
 hdfs dfs -mkdir /SSPProj
 wget https://x19155662timeseries.s3.amazonaws.com/OfficeData2.csv
 hdfs dfs -copyFromLocal OfficeData2.csv /SSPProj
