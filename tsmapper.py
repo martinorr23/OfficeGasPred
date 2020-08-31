05d1641a975758c9446bd89b1d9db2b5c8ce28af
martinorr23 <55511450+martinorr23@users.noreply.github.com>
2020-09-01 00:04:40 +0100

    Update tsmapper.py

--- tsmapper.py
+++ tsmapper.py
@@ -8,9 +8,9 @@ from datetime import datetime
 header_skipped = False
 
 for row in sys.stdin:
-    Timestamp, HDD, Gas, date, weekday, weekend, hour, outOfHours, month = row.rstrip(os.linesep).split(',')
+    Timestamp, HDD, Gas = row.rstrip(os.linesep).split(',')
     if header_skipped: # skip header
-        date_time = datetime.strptime(Timestamp, '%d/%m/%Y %H:%M')
+        date_time = datetime.strptime(Timestamp, '%Y-%m-%d %H:%M:%S')
         date_time_string = datetime.strftime(date_time, '%Y%m%d%H%M')
-        print('{}\t{}\t{}'.format(date_time_string, Gas))
+        print('{}\t{}'.format(date_time_string, Gas))
     header_skipped = True
