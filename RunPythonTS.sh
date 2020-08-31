e7bbd4c4019d4c7af324e5263964e2f16ea8cdca
martinorr23 <55511450+martinorr23@users.noreply.github.com>
2020-08-31 22:30:50 +0100

    Update RunPythonTS.sh

--- RunPythonTS.sh
+++ RunPythonTS.sh
@@ -4,8 +4,8 @@ chmod +x tsreducer.py
 hdfs dfs -rm -r /SSPProj
 hdfs dfs -rm -r /SSPProjOut
 hdfs dfs -mkdir /SSPProj
-wget https://x19155662timeseries.s3.amazonaws.com/OfficeData2.csv
-hdfs dfs -copyFromLocal OfficeData2.csv /SSPProj
+wget https://x19155662timeseries.s3.amazonaws.com/OfficeDataMR.csv
+hdfs dfs -copyFromLocal OfficeDataMR.csv /SSPProj
 hadoop jar /usr/lib/hadoop/hadoop-streaming.jar -files tsmapper.py,tsreducer.py,tscombiner.py -mapper tsmapper.py -reducer tsreducer.py -combiner tscombiner.py -input /SSPProj -output /SSPProjOut --cmdenv window_size=5
 #hadoop-streaming -file tsmapper.py -mapper tsmapper.py -file tscombiner.py -combiner tscombiner.py -file tsreducer.py -reducer tsreducer.py -input /SSPProj -output /SSPProjOut --cmdenv window_size=5
 rm part-00000
