b195112eb0b217af262ace1c0ed0a5228583e704
martinorr23 <55511450+martinorr23@users.noreply.github.com>
2020-08-31 22:10:58 +0100

    Update RunPythonTS.sh

--- RunPythonTS.sh
+++ RunPythonTS.sh
@@ -6,6 +6,7 @@ hdfs dfs -rm -r /SSPProjOut
 hdfs dfs -mkdir /SSPProj
 wget https://x19155662timeseries.s3.amazonaws.com/OfficeData2.csv
 hdfs dfs -copyFromLocal OfficeData2.csv /SSPProj
-hadoop-streaming -file tsmapper.py -mapper tsmapper.py -file tscombiner.py -combiner tscombiner.py -file tsreducer.py -reducer tsreducer.py -input /SSPProj -output /SSPProjOut --cmdenv window_size=5
+hadoop jar /usr/lib/hadoop/hadoop-streaming.jar -files tsmapper.py,tsreducer.py,tscombiner.py -mapper mapper.py -reducer reducer.py -combiner tscombiner.py -input /SSPProjOut -output /SSPProjOut --cmdenv window_size=5
+#hadoop-streaming -file tsmapper.py -mapper tsmapper.py -file tscombiner.py -combiner tscombiner.py -file tsreducer.py -reducer tsreducer.py -input /SSPProj -output /SSPProjOut --cmdenv window_size=5
 rm part-00000
 hdfs dfs -get /user/hduser/SSP/OfficeOut/part-00000 .
