07c5872255518bc5fdc9d07c0592737a0a3edd2b
martinorr23 <55511450+martinorr23@users.noreply.github.com>
2020-09-03 10:46:36 +0100

    Update RunPythonTS.sh

--- RunPythonTS.sh
+++ RunPythonTS.sh
@@ -7,7 +7,7 @@ hdfs dfs -mkdir /SSPProj
 wget https://x19155662timeseries.s3.amazonaws.com/OfficeDataMR.csv
 hdfs dfs -copyFromLocal OfficeDataMR.csv /SSPProj
 
-#Hadoop streaming command
+#Hadoop streaming command, window size:2
 hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \
 -file tsmapper.py \
 -file tsreducer.py \
@@ -16,9 +16,56 @@ hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \
 -reducer "python3 ./tsreducer.py" \
 -combiner "python3 ./tscombiner.py" \
 -input /SSPProj/OfficeDataMR.csv \
--output /SSPProjOut \
+-output /SSPProjOutTwo \
+-cmdenv window_size=2
+
+#Hadoop streaming command, window size:3
+hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \
+-file tsmapper.py \
+-file tsreducer.py \
+-file tscombiner.py \
+-mapper "python3 ./tsmapper.py" \
+-reducer "python3 ./tsreducer.py" \
+-combiner "python3 ./tscombiner.py" \
+-input /SSPProj/OfficeDataMR.csv \
+-output /SSPProjOutThree \
+-cmdenv window_size=3
+
+#Hadoop streaming command, window size:4
+hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \
+-file tsmapper.py \
+-file tsreducer.py \
+-file tscombiner.py \
+-mapper "python3 ./tsmapper.py" \
+-reducer "python3 ./tsreducer.py" \
+-combiner "python3 ./tscombiner.py" \
+-input /SSPProj/OfficeDataMR.csv \
+-output /SSPProjOutFour \
+-cmdenv window_size=4
+
+#Hadoop streaming command, window size:5
+hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \
+-file tsmapper.py \
+-file tsreducer.py \
+-file tscombiner.py \
+-mapper "python3 ./tsmapper.py" \
+-reducer "python3 ./tsreducer.py" \
+-combiner "python3 ./tscombiner.py" \
+-input /SSPProj/OfficeDataMR.csv \
+-output /SSPProjOutFive \
 -cmdenv window_size=5
 
-#hadoop-streaming -file tsmapper.py -mapper tsmapper.py -file tscombiner.py -combiner tscombiner.py -file tsreducer.py -reducer tsreducer.py -input /SSPProj -output /SSPProjOut --cmdenv window_size=5
-rm part-00000
-hdfs dfs -get /user/hduser/SSP/OfficeOut/part-00000 .
+#Hadoop streaming command, window size:6
+hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \
+-file tsmapper.py \
+-file tsreducer.py \
+-file tscombiner.py \
+-mapper "python3 ./tsmapper.py" \
+-reducer "python3 ./tsreducer.py" \
+-combiner "python3 ./tscombiner.py" \
+-input /SSPProj/OfficeDataMR.csv \
+-output /SSPProjOutSix \
+-cmdenv window_size=6
+
+hadoop fs -getmerge /SSPProjOutSix /home/hadoop/output/Six.csv
+#hdfs dfs -get /user/hduser/SSP//SSPProjOutSix/part-00000 .
